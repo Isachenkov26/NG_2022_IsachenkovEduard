945150ad7b6c68174bfeab45a9c6a69ae215cafd
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-04-16 12:29:51 +0300

    Improved the function, if the discriminant is negative, then use higher mathematics

--- Lesson_1/Task_1_5/main.cpp
+++ Lesson_1/Task_1_5/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-    double a, b, c, d, x1, x2;
+    double a, b, c, d, x1, x2, x1_1, x1_2, x2_2;
     cout << "Enter a: "; cin >> a;
     cout << "Enter b: "; cin >> b;
     cout << "Enter c: "; cin >> c;
@@ -17,7 +17,15 @@ int main()
         cout << "The first root: " << x1 << endl;
         cout << "the second root: " << x2 << endl;
     }
-    else if (d < 0) cout << "No roots";
+    else if (d < 0) {
+        cout << "The equation is negative, we use higher mathematics" << endl;
+        d = sqrt(-(d));
+        x1_1 = (-b/(2*a));
+        x1_2 = (-d/(2*a));
+        x2_2 = (d/(2*a));
+        cout << "The first root: " << x1_1 << x1_2 << "i" << endl;
+        cout << "the second root: " << x1_1 << "+" << x2_2 << "i" << endl;
+    }
 
     else if (d == 0){
         x1 = -(b/(2*a));
