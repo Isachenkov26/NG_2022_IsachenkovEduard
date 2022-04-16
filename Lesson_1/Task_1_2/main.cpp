4bde87fd84c0f336049f156a66447a89c2d278b7
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-04-16 11:02:13 +0300

    Added task_1_2

---
+++ Lesson_1/Task_1_2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << ",         ," << endl
+         << "|\\\\\\\\ ////|" << endl
+         << "| \\\\\\V/// |" << endl
+         << "|  |~~~|  |" << endl
+         << "|  |===|  |" << endl
+         << "|  |j  |  |" << endl
+         << "|  | g |  |" << endl
+         << " \\ |  s| / " << endl
+         << "  \\|===|/  " << endl
+         << "   '---'   " << endl;
+    return 0;
+}
