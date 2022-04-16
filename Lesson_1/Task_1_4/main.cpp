03457be56d2c6d3509f3e7a68e91c3b87d7d142e
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-04-16 23:35:29 +0300

    Enter the price of components for the PC, discount and get the result

--- Lesson_1/Task_1_4/main.cpp
+++ Lesson_1/Task_1_4/main.cpp
@@ -11,5 +11,6 @@ int main()
     cout << "Discount: "; cin >> discount;
     price = (100-discount)*0.01*(motherboard + videocard + cpu);
     cout << "Your PC will cost - " << price << '$';
+
     return 0;
 }
