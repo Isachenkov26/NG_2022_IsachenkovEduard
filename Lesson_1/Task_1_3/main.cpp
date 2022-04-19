be8d52a388ed40c7898b28f8dec0e894a53e47fc
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-04-19 16:05:24 +0300

    Fixed code style

--- Lesson_1/Task_1_3/main.cpp
+++ Lesson_1/Task_1_3/main.cpp
@@ -6,13 +6,18 @@ int main()
 {
     int output;
     double years;
-    cout << "Enter your age: "; cin >> years;
+    cout << "Enter your age: ";
+    cin >> years;
     if(years > 10){
-        cout << "Do you study? (1\\0): "; cin >> output;
-        if(output == 0) cout << "Why?";
-        else cout << "You are cool!";
+        cout << "Do you study? (1\\0): ";
+        cin >> output;
+        if(output == 0)
+            cout << "Why?";
+        else
+            cout << "You are cool!";
     }
-    else cout << "Wow you're so big!";
+    else
+        cout << "Wow you're so big!";
     return 0;
 
 }
