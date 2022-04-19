833d135867fcd37467be415709e68ada490338e5
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-04-19 16:10:40 +0300

    Deleted one variable

--- Lesson_1/Task_1_4/main.cpp
+++ Lesson_1/Task_1_4/main.cpp
@@ -4,13 +4,15 @@ using namespace std;
 
 int main()
 {
-    double motherboard, videocard, cpu, discount, price;
-    cout << "Motherboard price: "; cin >> motherboard;
-    cout << "Videocard price: "; cin >> videocard;
-    cout << "CPU price: "; cin >> cpu;
-    cout << "Discount: "; cin >> discount;
-    price = (100-discount)*0.01*(motherboard + videocard + cpu);
-    cout << "Your PC will cost - " << price << '$';
-
+    double motherboard, videocard, cpu, discount;
+    cout << "Motherboard price: ";
+    cin >> motherboard;
+    cout << "Videocard price: ";
+    cin >> videocard;
+    cout << "CPU price: ";
+    cin >> cpu;
+    cout << "Discount: ";
+    cin >> discount;
+    cout << "Your PC will cost - " << (100-discount)*0.01*(motherboard + videocard + cpu) << '$';
     return 0;
 }
