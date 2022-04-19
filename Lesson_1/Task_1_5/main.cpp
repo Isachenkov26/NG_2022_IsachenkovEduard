b0d2166b4381f2434e614df23fb34751ac3a8a0f
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-04-19 16:20:01 +0300

    Fixed code style and delete few variable

--- Lesson_1/Task_1_5/main.cpp
+++ Lesson_1/Task_1_5/main.cpp
@@ -5,33 +5,27 @@ using namespace std;
 
 int main()
 {
-    double a, b, c, d, x1, x2, x1_1, x1_2, x2_2;
-    cout << "Enter a: "; cin >> a;
-    cout << "Enter b: "; cin >> b;
-    cout << "Enter c: "; cin >> c;
+    double a, b, c, d;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b: ";
+    cin >> b;
+    cout << "Enter c: ";
+    cin >> c;
     d = (b*b) - 4*a*c;
     if(d > 0){
         d = sqrt(d);
-        x1 = (-b + d)/(2*a);
-        x2 = (-b - d)/(2*a);
-        cout << "The first root: " << x1 << endl;
-        cout << "the second root: " << x2 << endl;
+        cout << "The first root: " << (-b + d)/(2*a) << endl;
+        cout << "The second root: " << (-b - d)/(2*a) << endl;
     }
     else if (d < 0) {
         cout << "The equation is negative, we use higher mathematics" << endl;
-        d = sqrt(-(d));
-        x1_1 = (-b/(2*a));
-        x1_2 = (-d/(2*a));
-        x2_2 = (d/(2*a));
-        cout << "The first root: " << x1_1 << x1_2 << "i" << endl;
-        cout << "the second root: " << x1_1 << "+" << x2_2 << "i" << endl;
+        d = sqrt(-d);
+        cout << "The first root: " << (-b/(2*a)) << (-d/(2*a)) << "i" << endl;
+        cout << "The second root: " << (-b/(2*a)) << "+" << (d/(2*a)) << "i" << endl;
     }
-
     else if (d == 0){
-        x1 = -(b/(2*a));
-        cout << "The equation has one root: " << x1;
+        cout << "The equation has one root: " << -(b/(2*a)) << endl;
     }
-
     return 0;
-
 }
