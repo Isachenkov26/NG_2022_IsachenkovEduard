ae36905d39c000f4232194452689742c7a7cc4ca
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-04-20 15:26:12 +0300

    Search hash code with variable

---
+++ Lesson_2/Task_2_3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0, hash = 0, lastNumber = 0, tmpNumber = 0, num = 0;
+    cout << "Enter your number: ";
+    cin >> number;
+    num = number;
+    for(int i = num; i > 0; i /= 10){
+        tmpNumber = i / 10;
+        lastNumber = i % 10;
+        if((num % 10 == lastNumber && tmpNumber == 0) || (lastNumber == tmpNumber % 10))
+            hash += lastNumber;
+    }
+    cout << "Hash code with number(" << number << "): " << hash;
+    return 0;
+}
