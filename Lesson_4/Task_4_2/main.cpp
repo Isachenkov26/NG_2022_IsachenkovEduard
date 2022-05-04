a86366469360f4e1aaf2e245d9c0344cf0a5cd18
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-05-04 15:40:07 +0300

    Fixed bag

--- Lesson_4/Task_4_2/main.cpp
+++ Lesson_4/Task_4_2/main.cpp
@@ -5,14 +5,18 @@ using namespace std;
 int main()
 {
     char str[100];
-    cout << "Enter the string: ";
+    cout << "Enter the string: " << endl;
     cin.getline(str, 100);
     for(int i = 0; i < 100; i++){
         if(str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' || str[i] == 'Y' ||
             str[i] == 'a' || str[i] == 'e' || str[i] == 'i'  || str[i] == 'o'  || str[i] == 'u' || str[i] == 'y'){
             if(i % 2 == 0)
-                cout << str[i] << ' ';
+                cout << str[i];
+            else
+                cout << " ";
         }
+        else
+              cout << " ";
     }
     return 0;
 }
