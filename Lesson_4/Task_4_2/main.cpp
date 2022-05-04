db06427943d40c1ab45b87fc9bb0bd548c9ad954
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-05-04 17:14:54 +0300

    Fixe second bag

--- Lesson_4/Task_4_2/main.cpp
+++ Lesson_4/Task_4_2/main.cpp
@@ -10,13 +10,15 @@ int main()
     for(int i = 0; i < 100; i++){
         if(str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' || str[i] == 'Y' ||
             str[i] == 'a' || str[i] == 'e' || str[i] == 'i'  || str[i] == 'o'  || str[i] == 'u' || str[i] == 'y'){
-            if(i % 2 == 0)
-                cout << str[i];
+                if(i % 2 != 0){
+                    cout << str[i];
+                }
+                else{
+                    cout << " ";
+                }
+            }
             else
                 cout << " ";
-        }
-        else
-              cout << " ";
     }
     return 0;
 }
