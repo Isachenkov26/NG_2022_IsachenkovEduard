af8e785f13dd205416903b09b00981df8b2eb46c
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-05-04 15:33:18 +0300

    Deleted one variable

--- Lesson_2/Task_2_2/main.cpp
+++ Lesson_2/Task_2_2/main.cpp
@@ -4,15 +4,13 @@ using namespace std;
 
 int main()
 {
-    int number, num, suma = 0;
+    int number, suma = 0;
     cout << "Enter your number: ";
     cin >> number;
-    num = number;
-    while(num > 0){
-        if((num % 2) == 0){
-            suma += num % 10;
+    for(int i = number; i> 0; i /= 10){
+        if(i % 2 == 0){
+            suma += i % 10;
         }
-        num /= 10;
     }
     cout << "Sum of even numbers are: " << suma;
     return 0;
