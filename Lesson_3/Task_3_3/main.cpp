e55e9c5a007e75ca61e1c015abe6d8849374820b
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-05-06 13:21:32 +0300

    Fixed bag, Harbar Vadim helped me

--- Lesson_3/Task_3_3/main.cpp
+++ Lesson_3/Task_3_3/main.cpp
@@ -4,23 +4,22 @@ using namespace std;
 
 int main()
 {
-    int array[20], num;
+    int array[20]={0}, num = 0;
     for(int i = 0; i < 20; i++){
         cout << "Enter " << i << " element: ";
-        cin >> num;
-        if (num == 0)
+        cin >> array[i];
+        if(array[i] > num)
+            num = array[i];
+        if (array[i] == 0)
             break;
-        array[i] = num;
     }
-    for(int i = 0; i < 20; i++){
-        if(array[i] != 0){
-            for(int j = 0; j < array[i]; j++){
+    for(int i = 0; i < num; i++){
+        for(int j = 0; j < 20; j++){
+            if(array[j] > i)
                 cout << "*";
+           else
+                cout << " ";
             }
             cout << endl;
         }
-        else
-            break;
-    }
-    return 0;
 }
