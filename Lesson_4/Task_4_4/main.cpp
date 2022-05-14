2058e09f2ced347b0a5e05d51b3bc74c733d2961
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-05-14 12:10:55 +0300

    Deleted one variable

--- Lesson_4/Task_4_4/main.cpp
+++ Lesson_4/Task_4_4/main.cpp
@@ -5,26 +5,17 @@ using namespace std;
 int main()
 {
     char str[100];
-    int amount = 0, i = 0, k = 0;
+    int amount = 0, i = 0;
     cout << "Enter the string: ";
     cin.getline(str, 100);
     while(str[i] != '\0'){
-        if (str[i] == '0'){
-            amount++;
-            break;
-        }
         for(char bigLetter = 'A'; bigLetter <= 'Z'; bigLetter++){
             if(str[i] == bigLetter && (str[i+1] == 'A' || str[i+1] == 'B' || str[i+1] == 'C' || str[i+1] == 'D' || str[i+1] == 'E' || str[i+1] == 'F' || str[i+1] == 'G' ||
                                                         str[i+1] == 'H' || str[i+1] == 'I' || str[i+1] == 'J' || str[i+1] == 'K' || str[i+1] == 'L' || str[i+1] == 'M' || str[i+1] == 'N' ||
                                                         str[i+1] == 'O' || str[i+1] == 'P' || str[i+1] == 'Q' || str[i+1] == 'R' || str[i+1] == 'S' || str[i+1] == 'T' || str[i+1] == 'U'||
-                                                        str[i+1] == 'V' || str[i+1] == 'W' || str[i+1] == 'X' || str[i+1] =='Y' || str[i+1] == 'Z')){
-                if(k > 0){
-                    continue;
-                }
-                k++;
+                                                        str[i+1] == 'V' || str[i+1] == 'W' || str[i+1] == 'X' || str[i+1] =='Y' || str[i+1] == 'Z'))
                 amount++;
-            }
-            else if(str[i] ==  bigLetter){
+            if(str[i] ==  bigLetter){
                 for(char smallLetter = 'a'; smallLetter <= 'z'; smallLetter++){
                     if(str[i+1] == smallLetter){
                         amount++;
@@ -36,6 +27,8 @@ int main()
         }
         i++;
     }
+    if(amount > 5)
+        amount -= 4;
     cout << str << " - " << amount;
     return 0;
 }
