66e2150b43c9b01898934a24a0c3af07020693dd
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-05-14 12:37:48 +0300

    Fixed bag

--- Lesson_4/Task_4_2/main.cpp
+++ Lesson_4/Task_4_2/main.cpp
@@ -6,7 +6,11 @@ int main()
 {
     char str[100];
     cout << "Enter the string: " << endl;
-    cin.getline(str, 100);
+    do{
+        cin.getline(str, 100);
+        if(str[0] == ' ')
+            cout << "Enter the correct string: " << endl;
+    }while(str[0] == ' ');
     for(int i = 0; i < 100; i++){
         if(str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U' || str[i] == 'Y' ||
             str[i] == 'a' || str[i] == 'e' || str[i] == 'i'  || str[i] == 'o'  || str[i] == 'u' || str[i] == 'y'){
