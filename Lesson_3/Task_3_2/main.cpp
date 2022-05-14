9a26dfcf37ae84a0a8310c625ba1a950b1a67fdd
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-05-14 11:28:33 +0300

    Optimized the program by reducing the amount of manual work

--- Lesson_3/Task_3_2/main.cpp
+++ Lesson_3/Task_3_2/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int firstArray[4], secondArray[4], firstNum = 0, secondNum = 0;
+    int firstArray[4], secondArray[4], firstNum = 0, secondNum = 0, num = 1000;
     for(int i = 0; i < 4; i++){
         firstArray[i] = rand()%9;
         secondArray[i] = rand()%9;
@@ -20,20 +20,24 @@ int main()
     for(int i = 0; i < 4; i++)
         cout << " " << secondArray[i];
 
-    firstNum = firstArray[0] * 1000;
-    firstNum += (firstArray[1] * 100);
-    firstNum += (firstArray[2] * 10);
-    firstNum += firstArray[3] ;
+    for(int i = 0; i < 3; i++){
+        firstNum += firstArray[i] * num;
+        num /= 10;
+    }
+    firstNum += firstArray[3];
 
-    secondNum = secondArray[0] * 1000;
-    secondNum += (secondArray[1] * 100);
-    secondNum += (secondArray[2] * 10);
+    num = 1000;
+
+    for(int i = 0; i < 3; i++){
+        secondNum += secondArray[i] * num;
+        num /= 10;
+    }
     secondNum += secondArray[3];
 
     if(firstNum > secondNum)
         cout << endl << firstNum << " > " << secondNum;
     else if(firstNum < secondNum)
-            cout << endl << firstNum << " < " << secondNum;
+        cout << endl << firstNum << " < " << secondNum;
     else
         cout << endl << firstNum << " = "  << secondNum;
     return 0;
