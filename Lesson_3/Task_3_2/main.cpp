46404825c717d34dd583e18a474608da838f1b21
Isachenkov26 <isachenkov.eduard26@gmail.com>
2022-05-25 13:03:30 +0300

    Removed handwork, fixed program

--- Lesson_3/Task_3_2/main.cpp
+++ Lesson_3/Task_3_2/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int firstArray[4], secondArray[4], firstNum = 0, secondNum = 0, num = 1000;
+    int firstArray[4], secondArray[4], firstNum = 0, secondNum = 0;
     for(int i = 0; i < 4; i++){
         firstArray[i] = rand()%9;
         secondArray[i] = rand()%9;
@@ -20,20 +20,10 @@ int main()
     for(int i = 0; i < 4; i++)
         cout << " " << secondArray[i];
 
-    for(int i = 0; i < 3; i++){
-        firstNum += firstArray[i] * num;
-        num /= 10;
+    for(int i = 0, j = 1000; i < 4; i++, j /= 10){
+            firstNum += firstArray[i] * j;
+            secondNum += secondArray[i] * j;
     }
-    firstNum += firstArray[3];
-
-    num = 1000;
-
-    for(int i = 0; i < 3; i++){
-        secondNum += secondArray[i] * num;
-        num /= 10;
-    }
-    secondNum += secondArray[3];
-
     if(firstNum > secondNum)
         cout << endl << firstNum << " > " << secondNum;
     else if(firstNum < secondNum)
